75640dc4b7deeeba36be1dddd671f68b973637e8
jose alfredo <a00516283@gmail.com>
2018-01-09 21:16:08 -0600

    style post added

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -8,14 +8,39 @@ import {PostStorageService} from '../post-storage.service';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-	posts: PostModel[];
+  posts: PostModel[]; 
 
   constructor(private postStorageService: PostStorageService) { }
 
   ngOnInit() {
-  	this.postStorageService.getPosts().subscribe((posts: PostModel[]) => {
-  		this.posts = posts;
-  	});
+  	this.posts =  [
+    {
+    title: "Valentines day",
+    texto: "A black winter care away",
+    author: "Chester Benningtion"
+  }, 
+  {
+    title: "In the End",
+    texto: "I tried so hard and got so far, but in the end, it doesnt even matter",
+    author: "Mike Shinoda"
+  },
+  {
+    title: "little things give you away",
+    texto: "All you ever wanted, so wanna truly look up to you",
+    author: "Chester Benningtion"
+  },
+  {
+    title: "Adventure of the life time",
+    texto: "monitos  monitos y mas monitos seee",
+    author: "Coldplay"
   }
+  ];
+  }
+
+  /*
+    this.postStorageService.getPosts().subscribe((posts: PostModel[]) => {
+      this.posts = posts;
+    });
+  */
 
 }
